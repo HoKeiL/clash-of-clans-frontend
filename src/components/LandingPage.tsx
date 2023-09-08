fdbc89b1b6fd50e778d5cba141266164e61d0a2b
HoKeiL <hokeileung1106@gmail.com>
2023-09-08 16:52:30 +0100

    setup landing page

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,8 @@
+import { Heading } from "@chakra-ui/react";
+export function LandingPage(): JSX.Element {
+    return (
+        <div>
+            <Heading>Welcome</Heading>
+        </div>
+    );
+}
