070f6a00e610698f35389e3f477347247e70c1f3
HoKeiL <hokeileung1106@gmail.com>
2023-09-11 23:06:08 +0100

    added submit and delete options

--- src/components/ImportTeamsPage.tsx
+++ src/components/ImportTeamsPage.tsx
@@ -1,42 +1,59 @@
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 import {
+    Button,
     Input,
+    List,
+    ListItem,
     Table,
     TableContainer,
     Tbody,
     Td,
+    Text,
     Th,
     Thead,
     Tr,
 } from "@chakra-ui/react";
+import axios from "axios";
 import Papa from "papaparse";
-import { SetStateAction, useState } from "react";
+import { useEffect, useState } from "react";
+import { baseUrl } from "../Utils/baseURL";
+import { ReceivedData } from "./Tabs";
+import { fetchAllTeams } from "../fetchAllTeams";
 
 interface Data {
-    TeamName: string;
-    TeamCaptain: string;
-    Player1: string;
-    Player2: string;
-    Player3: string;
-    Player4: string;
-    Player5: string;
-    Player6: string;
+    teamname: string;
+    teamcaptain: string;
+    teamplayer1: string;
+    teamplayer2: string;
+    teamplayer3: string;
+    teamplayer4: string;
+    teamplayer5: string;
+    teamplayer6: string;
+}
+interface importTeamViewProps {
+    teamOptions: ReceivedData[];
+    setTeamOptions: React.Dispatch<React.SetStateAction<ReceivedData[]>>;
 }
 
-export function ImportTeamsPage(): JSX.Element {
+export function ImportTeamsPage({
+    teamOptions,
+    setTeamOptions,
+}: importTeamViewProps): JSX.Element {
     //State to store table Column name
     const [tableRows, setTableRows] = useState<string[]>([]);
-
     //State to store the values
     const [values, setValues] = useState<string[]>([]);
+
+    const [isClicked, setIsClicked] = useState(false);
 
     const changeHandler = (event) => {
         // Passing file data (event.target.files[0]) to parse using Papa.parse
         Papa.parse(event.target.files[0], {
             header: true,
             skipEmptyLines: true,
-            complete: function (results) {
-                const rowsArray: SetStateAction<string[]>[] = [];
-                const valuesArray: SetStateAction<string[]>[] = [];
+            complete: function (results: Papa.ParseResult<[Data]>) {
+                const rowsArray: string[] = [];
+                const valuesArray: string[] = [];
 
                 // Iterating data to get column name and their values
                 results.data.map((d) => {
@@ -53,39 +70,107 @@ export function ImportTeamsPage(): JSX.Element {
         });
     };
 
+    useEffect(() => {
+        fetchAllTeams().then((uploadedTeams) => setTeamOptions(uploadedTeams));
+    }, [isClicked === true]);
+
+    async function handleSumbitTeams() {
+        setIsClicked(true);
+        await addNewTeams();
+        setIsClicked(false);
+    }
+
+    async function addNewTeams() {
+        for (let i = 0; i < values.length; i++) {
+            await axios.post(`${baseUrl}/teams`, generateTeams(i));
+        }
+    }
+    function generateTeams(i: number) {
+        return {
+            teamname: values[i][0],
+            teamcaptain: values[i][1],
+            teamplayer1: values[i][2],
+            teamplayer2: values[i][3],
+            teamplayer3: values[i][4],
+            teamplayer4: values[i][5],
+            teamplayer5: values[i][6],
+            teamplayer6: values[i][7],
+        };
+    }
+
+    async function handleDeleteTeams() {
+        setIsClicked(true);
+        await deleteTeams();
+        setIsClicked(false);
+    }
+    async function deleteTeams() {
+        await axios.delete(`${baseUrl}/teams`);
+    }
+
     return (
         <div>
             <Input
                 type="file"
-                name="file"
                 accept=".csv"
                 onChange={changeHandler}
                 margin={"1em"}
                 variant="flushed"
             />
-
-            <TableContainer marginTop={"1em"}>
-                <Table variant="simple">
-                    <Thead>
-                        <Tr>
-                            {tableRows.map((rows, index) => {
-                                return <Th key={index}>{rows}</Th>;
-                            })}
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {values.map((value, index) => {
-                            return (
-                                <Tr key={index}>
-                                    {value.map((values, index) => {
-                                        return <Td key={index}>{values}</Td>;
+            {values.length > 0 && (
+                <div>
+                    <TableContainer marginTop={"1em"}>
+                        <Table variant="simple">
+                            <Thead>
+                                <Tr>
+                                    {tableRows.map((rows, index) => {
+                                        return <Th key={index}>{rows}</Th>;
                                     })}
                                 </Tr>
-                            );
-                        })}
-                    </Tbody>
-                </Table>
-            </TableContainer>
+                            </Thead>
+                            <Tbody>
+                                {values.map((value, index) => {
+                                    return (
+                                        <Tr key={index}>
+                                            {value.map((rowValues, index) => {
+                                                return (
+                                                    <Td key={index}>
+                                                        {rowValues}
+                                                    </Td>
+                                                );
+                                            })}
+                                        </Tr>
+                                    );
+                                })}
+                            </Tbody>
+                        </Table>
+                    </TableContainer>
+                    <Button
+                        margin={"1em"}
+                        rightIcon={<ArrowForwardIcon />}
+                        onClick={handleSumbitTeams}
+                        isLoading={isClicked}
+                        loadingText={isClicked && "Submitting"}
+                        variant={isClicked ? "outline" : "solid"}
+                    >
+                        Submit Teams
+                    </Button>
+                </div>
+            )}
+            <Text margin={"1em"}>Currently uploaded teams: </Text>
+            <List margin={"1em"}>
+                {teamOptions.map((team) => (
+                    <ListItem key={team.id}>{team.teamname}</ListItem>
+                ))}
+            </List>
+            <Button
+                margin={"1em"}
+                onClick={handleDeleteTeams}
+                isLoading={isClicked}
+                loadingText={isClicked && "Deleting"}
+                variant={isClicked ? "outline" : "solid"}
+            >
+                Delete
+            </Button>
         </div>
     );
 }
