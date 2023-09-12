62f31aae599e8bbc0c50ab0241810fc6422483f9
HoKeiL <hokeileung1106@gmail.com>
2023-09-12 03:29:36 +0100

    updated landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,8 +1,48 @@
-import { Heading } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Heading,
+    Image,
+    Text,
+    Wrap,
+    WrapItem,
+} from "@chakra-ui/react";
+
 export function LandingPage(): JSX.Element {
     return (
-        <div>
-            <Heading>Welcome</Heading>
-        </div>
+        <Wrap justify="center">
+            <WrapItem>
+                <Box
+                    w="100%"
+                    h="400px"
+                    textAlign="left"
+                    // border="1px"
+                    // borderColor="black"
+                    paddingLeft="2em"
+                >
+                    <Heading as="h1" size="4xl" mt="2em">
+                        Welcome
+                    </Heading>
+                    <Text as="h2" fontSize="lg" mt="1em">
+                        Broken String Special Session: Clash of Clans
+                    </Text>
+                    <Button mt="24px"> Let's go</Button>
+                </Box>
+            </WrapItem>
+            <WrapItem>
+                <Box
+                    w="100%"
+                    h="400px"
+                    // border="1px"
+                    // borderColor="black"
+                >
+                    <Image
+                        src="https://secure.meetupstatic.com/photos/event/9/f/3/8/600_514780760.webp?w=384
+"
+                        alt="Clash of Clans Logo"
+                    />
+                </Box>
+            </WrapItem>
+        </Wrap>
     );
 }
