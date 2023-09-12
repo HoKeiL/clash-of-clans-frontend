dc6d169d29eef6f4668f21a9a5bc7d97811119a8
HoKeiL <hokeileung1106@gmail.com>
2023-09-12 03:29:52 +0100

    spell check

--- src/fetchAllTeams.ts
+++ src/fetchAllTeams.ts
@@ -3,8 +3,8 @@ import { baseUrl } from "./Utils/baseURL";
 
 export async function fetchAllTeams() {
     try {
-        const reponse = await axios.get(`${baseUrl}/teams`);
-        const result = reponse.data;
+        const response = await axios.get(`${baseUrl}/teams`);
+        const result = response.data;
         return result;
     } catch (error) {
         console.log(error);
