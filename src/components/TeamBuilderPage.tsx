79017d343e454779157400765947ef13e51bca4e
HoKeiL <hokeileung1106@gmail.com>
2023-10-17 19:34:02 +0100

    added axios post

--- src/components/TeamBuilderPage.tsx
+++ src/components/TeamBuilderPage.tsx
@@ -11,7 +11,9 @@ import {
     Text,
     UnorderedList,
 } from "@chakra-ui/react";
+import axios from "axios";
 import { useState } from "react";
+import { baseUrl } from "../Utils/baseURL";
 import { ReceivedData } from "../Utils/interfaces";
 import {
     DisplayTeamPlayers,
@@ -27,6 +29,24 @@ export type AlertMessages =
     | "Please check all pairings are unique"
     | "Please check if all individual pairing is unique"
     | "success ";
+
+export interface OrderOfPlayData {
+    teamname: string;
+    game1Player1: string;
+    game1Player2: string;
+    game2Player1: string;
+    game2Player2: string;
+    game3Player1: string;
+    game3Player2: string;
+    game4Player1: string;
+    game4Player2: string;
+    game5Player1: string;
+    game5Player2: string;
+    game6Player1: string;
+    game6Player2: string;
+    game7Player1: string;
+    game7Player2: string;
+}
 
 export interface teamBuilderProps {
     teamOptions: ReceivedData[];
@@ -44,6 +64,7 @@ export function TeamBuilderPage({
     const [alertMessage, setAlertMessage] = useState<
         AlertMessages | undefined
     >();
+    const [isClicked, setIsClicked] = useState(false);
 
     function createInitialEmptyPair(numOfTeamPlayers: number): string[] {
         const initialArray = [];
@@ -60,6 +81,40 @@ export function TeamBuilderPage({
 
         setChosenTeam(team);
         setTeamPlayers(playersOnlyArray);
+        setSelectedPlayers(createInitialEmptyPair(7));
+        setIsPassed(undefined);
+        setAlertMessage(undefined);
+    }
+
+    async function handleSubmitOrderOfPlay() {
+        console.log(
+            " chosenTeam " +
+                chosenTeam![0].teamname +
+                " selected Pairings: " +
+                selectedPlayers
+        );
+        setIsClicked(true);
+        const dataObject: OrderOfPlayData = {
+            teamname: chosenTeam![0].teamname,
+            game1Player1: selectedPlayers[0],
+            game1Player2: selectedPlayers[1],
+            game2Player1: selectedPlayers[2],
+            game2Player2: selectedPlayers[3],
+            game3Player1: selectedPlayers[4],
+            game3Player2: selectedPlayers[5],
+            game4Player1: selectedPlayers[6],
+            game4Player2: selectedPlayers[7],
+            game5Player1: selectedPlayers[8],
+            game5Player2: selectedPlayers[9],
+            game6Player1: selectedPlayers[10],
+            game6Player2: selectedPlayers[11],
+            game7Player1: selectedPlayers[12],
+            game7Player2: selectedPlayers[13],
+        };
+        await axios.post(`${baseUrl}/orderOfPlay`, dataObject);
+        console.log("posted");
+
+        setIsClicked(false);
     }
 
     return (
@@ -165,7 +220,12 @@ export function TeamBuilderPage({
                                             }
                                         )}
                                     </OrderedList>
-                                    <Button mt="1em" mb="0.5em">
+                                    <Button
+                                        mt="1em"
+                                        mb="0.5em"
+                                        onClick={handleSubmitOrderOfPlay}
+                                        isLoading={isClicked}
+                                    >
                                         Ready to submit
                                     </Button>
                                 </>
